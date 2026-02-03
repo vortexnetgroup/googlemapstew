01621ab85b791f524052666d0e059cc53180693d
Dorker <142069939+meowgoober@users.noreply.github.com>
2026-02-02 17:15:08 -0700

    fuck it remove it all

--- src/googlemapstew/__init__.py
+++
@@ -1,8 +0,0 @@
-"""googlemapstew - Basic Google Maps information scraper using BeautifulSoup4."""
-
-__version__ = "0.1.0"
-__author__ = "Your Name"
-
-from googlemapstew.scraper import GoogleMapsScraper
-
-__all__ = ["GoogleMapsScraper", "__version__"]
